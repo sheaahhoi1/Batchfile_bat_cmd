664a45205a2c2e6531ef034344a292701a680531
sheaahhoi1 <sheaahhoi66@gmail.com>
2017-09-12 20:07:31 +0800

    Add files via upload

---
+++ 個人項目/SSD Disk安全捷徑MK.cmd
@@ -0,0 +1,14 @@
+md "R:\SSD_fast_link"
+
+mklink /j "R:\SSD_fast_link\um_Program_Files_64\World of Warcraft\Cache" "E:\um_Program_Files\World of Warcraft\Cache"
+
+#mklink /j "R:\SSD_fast_link\um_Program_Files_64\World of Warcraft\Interface" "E:\um_Program_Files\World of Warcraft\Interface"
+
+#mklink /j "R:\SSD_fast_link\um_Program_Files_64\World of Warcraft\Screenshots" "E:\um_Program_Files\World of Warcraft\Screenshots"
+
+#mklink /j "R:\SSD_fast_link\um_Program_Files_64\World of Warcraft\WTF" "E:\um_Program_Files\World of Warcraft\WTF"
+
+pause
+#md "R:\fast_link"
+#mklink "R:\fast_link\TTPlayer.exe" "E:\um_Program_Files\TTPlayer\TTPlayer.exe"
+pause
